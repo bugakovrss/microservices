d3e370d91ecf35e61e2e14b3c5db6c5c2534b84b
Roman Bugakov <roman.bugakov@simbirsoft.com>
2020-11-03 00:04:18 +0400

    Task-3 Add eventlog service project

---
+++ SmartHome.EventlogApi/ErrorHandling/ErrorHandlingMiddleware.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using SmartHome.Model.Errors;
+
+namespace SmartHome.EventlogApi.ErrorHandling
+{
+    /// <summary>
+    /// Обработчик ошибок
+    /// </summary>
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        private readonly IOptions<MvcNewtonsoftJsonOptions> _jsonOptions;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="next">Делегат</param>
+        /// <param name="logger"></param>
+        /// <param name="jsonOptions">JSON опции</param>
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger, IOptions<MvcNewtonsoftJsonOptions> jsonOptions)
+        {
+            _next = next;
+            _logger = logger;
+            _jsonOptions = jsonOptions;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode code;
+            object errorObjeсt;
+
+            switch (exception)
+            {
+                case EventlogApiException ex:
+                {
+                    errorObjeсt = ex.GetError();
+                    code = HttpStatusCode.BadRequest;
+                    break;
+                }
+                default:
+                {
+                    string absoluteURL = $"{context.Request.Scheme}://{context.Request.Host}{context.Request.Path}{context.Request.QueryString}";
+                    _logger.LogError(exception, $"Произошла непредвиденная ошибка: {absoluteURL}");
+                    errorObjeсt = new ErrorModel { Error = exception.Message, ErrorCode = ErrorCode.UnspecifiedError };
+                    code = HttpStatusCode.InternalServerError;
+                    break;
+                }
+            }
+
+            var result = JsonConvert.SerializeObject(errorObjeсt,Formatting.Indented, _jsonOptions.Value.SerializerSettings );
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+	}
+}
---
+++ SmartHome.EventlogApi/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmartHome.EventlogApi.ErrorHandling;
+using SmartHome.EventlogApi.WebExtensions;
+using SmartHome.Model;
+using SmartHome.Model.Entities;
+using SmartHome.Model.Initializers;
+using Steeltoe.Discovery.Client;
+
+namespace SmartHome.EventlogApi
+{
+    public class Startup
+    {
+        private const string ApplicationName = "smarthome-eventlogapi-app";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDiscoveryClient(Configuration);
+
+            services.AddMvcCore().AddResponseFormatters();
+
+            services.AddSwaggerEx(ApplicationName);
+
+            services.AddSingleton<IRepository<Device>>(ioc => new Repository<Device>(DeviceInitializer.Initialize()));
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider versionProvider)
+        {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
+            app.UseDiscoveryClient();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUiEx(versionProvider);
+        }
+    }
+}
