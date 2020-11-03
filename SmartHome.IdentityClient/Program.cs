08e555f53450e730589024def8d9898a40d3e443
Roman Bugakov <roman.bugakov@simbirsoft.com>
2020-11-04 01:17:25 +0400

    Task-4 Add identity service

---
+++ SmartHome.IdentityClient/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace SmartHome.IdentityClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var data = RequestDeviceEventsWithPolicy().Result;
+            Console.ReadLine();
+        }
+
+        public static async Task<string> RequestDeviceEventsWithPolicy()
+        {
+
+            using (var client = new HttpClient())
+            {
+
+                var token = await GetRefreshTokenAsync(client, "http://localhost:5000", "client", "secret", "eventlogapi");
+                client.SetBearerToken(token);
+
+                var response = await client.GetAsync("http://localhost:8090/api/v1/DeviceEvents?deviceId=1");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return response.StatusCode.ToString();
+                }
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                return content;
+            }
+        }
+
+        private static async Task<string> GetRefreshTokenAsync( HttpClient client, string authority, string clientId, string secret, string apiName)
+        {
+            var disco = await client.GetDiscoveryDocumentAsync(authority);
+            if (disco.IsError) throw new Exception(disco.Error);
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = clientId,
+                ClientSecret = secret,
+                Scope = apiName
+            });
+
+            if (!tokenResponse.IsError) 
+                return tokenResponse.AccessToken;
+
+            var error = tokenResponse.Error;
+            return null;
+        }
+    }
+}
---
+++ SmartHome.IdentityServer/Startup.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SmartHome.IdentityServer.Configurations;
+
+namespace SmartHome.IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients)
+                .AddDeveloperSigningCredential();
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
