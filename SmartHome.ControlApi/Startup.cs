13a930ac3c7b3a2e57a423fc5b1b415af77b327d
Roman Bugakov <60781265+bugakovrss@users.noreply.github.com>
2020-11-08 22:50:28 +0400

    task-5 Add circuitbreaker and retry patterns (#4)

--- SmartHome.ControlApi/Startup.cs
+++ SmartHome.ControlApi/Startup.cs
@@ -1,10 +1,15 @@
+using System;
+using System.Net;
 using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Timeout;
 using SmartHome.ControlApi.Configuration;
 using SmartHome.ControlApi.ErrorHandling;
 using SmartHome.ControlApi.Services;
@@ -19,6 +24,7 @@ namespace SmartHome.ControlApi
 {
     public class Startup
     {
+        private ILogger<Startup> _logger;
         private const string ApplicationName = "smarthome-controlapi-app";
 
         public Startup(IConfiguration configuration)
@@ -31,6 +37,7 @@ namespace SmartHome.ControlApi
 
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDiscoveryClient(Configuration);
 
             services.AddMvcCore().AddResponseFormatters();
@@ -73,13 +80,58 @@ namespace SmartHome.ControlApi
 
             // Configure HttpClient
             services.AddHttpClient<HttpClient>("eventlogs")
+                .SetHandlerLifetime(TimeSpan.FromMinutes(5))
                 .AddServiceDiscovery()
-                .AddTypedClient<IEventlogService, EventlogService>();
+                .AddTypedClient<IEventlogService, EventlogService>()
+                .AddPolicyHandler(GetRetryPolicy())
+                .AddPolicyHandler(GetCircuitBreakerPolicy())
+                .AddPolicyHandler(GetTimeoutPolicy());
         }
 
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider versionProvider)
+        private  IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
         {
+            return HttpPolicyExtensions.HandleTransientHttpError()
+                .Or<TimeoutRejectedException>()
+                .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+                .OrResult(msg => msg.StatusCode == HttpStatusCode.InternalServerError)
+                .WaitAndRetryAsync(2, attempt =>
+                        TimeSpan.FromSeconds(0.1 * Math.Pow(2, attempt)),
+                    (ex, duration) =>
+                    {
+                        _logger?.LogError(
+                            $"Error on send query to service. Retry through '{duration.TotalMilliseconds}' ms. '{ex.Result?.StatusCode}':'{ex.Exception?.Message}' ");
+                    });
+        }
+
+        private IAsyncPolicy<HttpResponseMessage> GetTimeoutPolicy()
+        {
+            return Policy.TimeoutAsync<HttpResponseMessage>(6);
+        }
+
+        private IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .Or<TimeoutRejectedException>()
+                .CircuitBreakerAsync(3,
+                    TimeSpan.FromSeconds(20),
+                    (ex, breakDelay) => { _logger?.LogError($"CirketBreaker '{breakDelay.TotalMilliseconds}' StatusCode'{ex.Result?.StatusCode}':{ex.Exception?.Message}"); },
+                    () =>
+                    {
+                        _logger.LogInformation(".Breaker logging: Call ok! Closed the circuit again!");
+                    },
+                    () =>
+                    {
+                        _logger.LogInformation(".Breaker logging: Half-open: Next call is a trial!");
+                    }
+                    );
+        }
+
+        public void Configure(IApplicationBuilder app, IApiVersionDescriptionProvider versionProvider)
+        {
+            _logger = app.ApplicationServices.GetService<ILogger<Startup>>();
+            _logger.LogInformation("Запуск....");
+
             app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseDiscoveryClient();
